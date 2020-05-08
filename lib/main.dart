7a841feca52d7bb60d4704a0c973db0bca5c9b04
Mohammed Arshu PT <34481775+arshupt@users.noreply.github.com>
2020-05-08 16:48:54 +0530

    Home Window added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,106 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo List',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
+      title: 'Todo',
+      theme: ThemeData.dark().copyWith(
+        accentColor: Colors.red,
       ),
-      home: MyHomePage(title: 'ToDo'),
+      home: todo(),
     );
   }
 }
 
+class todo extends StatefulWidget {
+  @override
+  _todouiState createState() => _todouiState();
+}
+
+class _todouiState extends State<todo>{
+
+  Widget mycard(String task){
+    return Card(
+      elevation: 5.0,
+      margin: EdgeInsets.symmetric(
+        horizontal: 10.0,
+        vertical: 5.0,
+      ),
+      child: Container(
+        padding: EdgeInsets.all(5.0),
+        child: ListTile(
+          title: Text(
+            "$task",
+          ),
+          onLongPress: (){},
+        ),
+      ),
+    );
+  }
+  @override
+
+  void showalertdialog(){
+    showDialog(
+        context: context,
+        builder: (context)=> AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0)
+          ),
+          title: Text(
+            "Add Task",
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(),
+              Row(children: <Widget>[
+                RaisedButton(
+                  onPressed: (){},
+                  child: Text(
+                      "ADD"
+                  ),
+                )
+              ],)
+            ],
+          ),
+        ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: showalertdialog,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.red,
+      ),
+      appBar: AppBar(
+        title: Text("ToDo"),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            mycard("Meeting"),
+            mycard("Go out")
+          ],
+        ),
+      ),
+    );
+
+  }
+}
 
