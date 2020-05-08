8342ef6dcb4f7870aedc1965c0ee3eae3556765c
alkka <alkkawilson@gmail.com>
2020-05-08 10:17:57 +0530

    color changed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo List',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'ToDo'),
     );
